5c79bccfb208444b5e0ba5c0c049414339c4c987
chirag oza <ozachirag22@gmail.com>
2024-10-15 21:10:46 +0530

    navbar implementation of the ecommerce website

---
+++ frontend/src/common components/Navbar.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import styles from "../styles/navbar.module.css";
+import { Logo } from "./Logo";
+
+export const Navbar = () => {
+    return (
+        <div className={styles.navbarContainer}>
+            <Logo />
+            <div className={styles.searchBarContainer}>
+                <input type="text" placeholder="search the product here...." />
+                <button className={styles.searchBarButton}>
+                    <span className="material-symbols-outlined">search</span>
+                </button>
+            </div>
+            <div className={styles.quickAccessSection}>
+                <span className="material-symbols-outlined">favorite</span>
+                <span className="material-symbols-outlined">shopping_cart</span>
+                <span className="material-symbols-outlined">person</span>
+            </div>
+        </div>
+    )
+}
