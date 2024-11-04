59ca1f5a033aa0b23c3f8e735bf63433ef18790f
chirag oza <ozachirag22@gmail.com>
2024-11-04 15:13:01 +0530

    ref #4 : complete implementation of the homepage

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -6,6 +6,7 @@ import Image from "next/image";
 import image1 from "../../public/images/home/browsing image.png";
 import { TrendingSection } from "@/components/home/TrendingSection";
 import { Newsletter } from "@/components/home/NewsletterSection";
+import { RecommendationSection } from "@/components/home/RecommendationSystem";
 
 export default function Home() {
   return (
@@ -15,6 +16,7 @@ export default function Home() {
       <FeaturesCardSection />
       <TrendingSection />
       <Image src={image1} alt="image 1" className={styles.image} />
+      <RecommendationSection />
       <Newsletter />
     </div>
   );
